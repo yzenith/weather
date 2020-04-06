4fb159eb6e00e796768efc852fdbc063df3dff96
yzenith <yzenithcorp@gmail.com>
2020-04-06 16:35:55 -0500

    fix inital setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan("normal"));
+app.use(express.static("public"))
+
+app.get("/", (req,res)=>{
+  res.send(_dir/index.html);
+})
+
+
+let server = process.env.PORT || 8080;
+app.listen(PORT, ()=>{
+  console.log(`server is running on ${server}`)
+})
