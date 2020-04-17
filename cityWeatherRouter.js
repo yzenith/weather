b16483f2fa38ab15b91ff073470e5eb6ae476564
yzenith <yzenithcorp@gmail.com>
2020-04-17 18:01:13 -0500

    add city params

--- cityWeatherRouter.js
+++ cityWeatherRouter.js
@@ -26,5 +26,25 @@ router.get('/', (req,res)=>{
     })
 })
 
+router.get('/:city', (req,res)=>{
+
+    // request openweather GET method
+    let city = req.params.city;
+    let appid = "7fd64bea57746b38d50d97687525f21a";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}`;
+    return new Promise(function(resolve, reject){
+      request.get(url, function (err, response, body) {
+        if(err){
+          reject(error);
+        } else {
+          // console.log(`Body is: ${body}`) it will return the json body
+          resolve(
+            res.status(200).json(body)
+          );
+        }
+      })
+    })
+})
+
 
 module.exports = router;
